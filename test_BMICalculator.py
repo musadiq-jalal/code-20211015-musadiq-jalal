3d3e811ae3962604f3fb5b732eb3539561224e4d
Musadiq Jalal <musadiq.jalal@trialx.com>
2021-10-13 17:41:50 +0530

    added tests

--- test_BMICalculator.py
+++ test_BMICalculator.py
@@ -3,28 +3,20 @@ from .BMICalculator import bmi_calculator
 
 
 def test_bmi_calculator():
-    assert bmi_calculator(
-        {'Gender': "Male", 
-         'Height': 173, 
-         'Weight': 69.1}) == {'Gender': 'Male', 'Height': 173,'Weight': 69.1, 
+     result = bmi_calculator({'Gender': "Male", 'Height': 173,'Weight': 69.1})      
+     assert result == {'Gender': 'Male', 'Height': 173,'Weight': 69.1, 
                             'BMI': 23.1, 'BMI Category': 'Normal weight', 
                             'Health risk': 'Low risk'}
 
-    assert bmi_calculator(
-         {'Gender': "Female", 
-                'Height': 160, 
-                'Weight': 69.1}) == {'Gender': 'Female','Height': 160, 'Weight': 69.1, 
-                                'BMI': 27.0, 'BMI Category': 'Overweight',
-                                'Health risk': 'Enhanced risk'}
-    assert bmi_calculator(
-         {'Gender': "Male", 
-          'Height': 165, 
-          'Weight': 91.1}) == {'Gender': 'Male','Height': 165,'Weight': 91.1,
-                                 'BMI': 33.5,'BMI Category': 'Moderately obese',
-                                 'Health risk': 'Medium risk'}
-    assert bmi_calculator(
-         {'Gender': "Male", 
-          'Height': 173, 
-        'Weight': 119.1}) == {'Gender': 'Male', 'Height': 173, 'Weight': 119.1,
-                                    'BMI': 39.8,'BMI Category': 'Severely obese',
-                                    'Health risk': 'High risk'}
+def test_bmi_calculator_2():
+     result_2 = bmi_calculator({'Gender': "Female", 'Height': 160, 'Weight': 69.1}) 
+     assert result_2.get("Weight") == 69.1
+
+def test_bmi_calculator_3():
+    result_3 = bmi_calculator({'Gender': "Male", 'Height': 165, 'Weight': 91.1}) 
+    assert result_3.get("BMI Category") == "Moderately obese"
+
+def test_bmi_calculator_4():
+   result_4 = bmi_calculator({'Gender': "Male", 'Height': 173,'Weight': 119.1})
+   assert isinstance(result_4["BMI"],float) 
+# 
