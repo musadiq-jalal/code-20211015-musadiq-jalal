668f94695347dde88a9b0190c29c7ca0c40a7cec
Musadiq Jalal <musadiq.jalal@trialx.com>
2021-10-13 19:57:17 +0530

    changed naming conventions

--- test_BMICalculator.py
+++ test_BMICalculator.py
@@ -2,21 +2,21 @@ import pytest
 from .BMICalculator import bmi_calculator
 
 
-def test_bmi_calculator():
+def test_bmi_calculator_results():
      result = bmi_calculator({'Gender': "Male", 'Height': 173,'Weight': 69.1})      
      assert result == {'Gender': 'Male', 'Height': 173,'Weight': 69.1, 
                             'BMI': 23.1, 'BMI Category': 'Normal weight', 
                             'Health risk': 'Low risk'}
 
-def test_bmi_calculator_2():
-     result_2 = bmi_calculator({'Gender': "Female", 'Height': 160, 'Weight': 69.1}) 
-     assert result_2.get("Weight") == 69.1
+def test_bmi_calculator_weight():
+     result = bmi_calculator({'Gender': "Female", 'Height': 160, 'Weight': 69.1}) 
+     assert result.get("Weight") == 69.1
 
-def test_bmi_calculator_3():
-    result_3 = bmi_calculator({'Gender': "Male", 'Height': 165, 'Weight': 91.1}) 
-    assert result_3.get("BMI Category") == "Moderately obese"
+def test_bmi_calculator_category():
+    result = bmi_calculator({'Gender': "Male", 'Height': 165, 'Weight': 91.1}) 
+    assert result.get("BMI Category") == "Moderately obese"
 
-def test_bmi_calculator_4():
-   result_4 = bmi_calculator({'Gender': "Male", 'Height': 173,'Weight': 119.1})
-   assert isinstance(result_4["BMI"],float) 
+def test_bmi_calculator_bmi():
+   result = bmi_calculator({'Gender': "Male", 'Height': 173,'Weight': 119.1})
+   assert isinstance(result["BMI"],float) 
 # 
